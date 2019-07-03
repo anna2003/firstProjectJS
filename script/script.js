2f0c55dad764246ce73a62f0cec7f34ea7d915bd
Anna <ryavkina@mail.ru>
2019-07-03 21:27:46 +0500

    First commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+'use strict';
+let a=5;
+console.log(a);
+var firstVar=1;
+
+let secondVar=2;
+const pi=3.1415;
+console.log(secondVar,pi);
+let array=['first',1,2,true];
+console.log(array);
+let obj={
+    name:"Jon",
+    age:18,
+    isMarried:true
+};
+console.log(obj.age);
+let s="name";
+console.log(obj[s]);
